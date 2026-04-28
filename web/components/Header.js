aeabc2356d4459ba7c43a11641e37d6b14073816
Froxy-13 <alphadialloo3000@gmail.com>
2026-04-28 13:38:18 +0000

    feat: navigation vers commandes + bouton Commander

--- web/components/Header.js
+++ web/components/Header.js
@@ -1,17 +1,19 @@
 import Link from 'next/link';
 
 export default function Header() {
-  return (
-    <header className="header">
-      <div className="header-content">
-        <Link href="/" className="header-logo">
-          🍛 Terranga<span>Food</span>
-        </Link>
-        <h4>Hextech</h4>
-        <nav className="header-nav">
-          <Link href="/">Restaurants</Link>
-        </nav>
-      </div>
-    </header>
-  );
+return (
+<header className="header">
+<div className="header-content">
+<Link href="/" className="header-logo">
+Terranga<span>Food</span>
+</Link>
+<nav className="header-nav">
+<Link href="/">Restaurants</Link>
+<Link href="/mes-commandes">
+Mes commandes
+</Link>
+</nav>
+</div>
+</header>
+);
 }
