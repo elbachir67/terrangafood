9a186767fff7fdf6a4f9615bcbf8c3f4efe33a9d
tbyb <boyethiane27@gmail.com>
2026-05-12 23:32:25 +0000

    fix: manual cors headers

--- api/src/app.js
+++ api/src/app.js
@@ -51,9 +51,16 @@ const corsOptions = {
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true  // Si vous utilisez des cookies/authentification
 };
-app.use(cors(corsOptions));
-
-app.options('*', cors(corsOptions)); 
+ 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
 
 // --- Middleware globaux ---
 
